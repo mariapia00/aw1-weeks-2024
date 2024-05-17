2d1a9cc1d16fa43f9a9fbfb8c0649f7c9f04c728
Maria Pia Abbondandolo <s331355@studenti.polito.it>
2024-05-17 18:06:06 +0200

    aggiumta commenti settimana 1 e stampa in ordine alfabetico

--- week01/exercises/scores.js
+++ week01/exercises/scores.js
@@ -1,5 +1,5 @@
 'use strict';
-
+ //Embed the scores directly in the source code.
 const scores = [20, -5, -1, 100, -3, 30, 50];
 const betterScores = [];
 let NN = 0;
@@ -11,6 +11,8 @@ for(const s of scores) {
 
 NN = scores.length - betterScores.length;
 
+
+// Eliminate the two lowest-ranking scores.
 // VERSIONE CON MIN
 /*
 let minScore = Math.min(...betterScores);
@@ -22,21 +24,42 @@ index = betterScores.indexOf(minScore);
 betterScores.splice(index, 1);
 */
 // VERSIONE CON SORT
+
+/*
+Eliminate the two lowest-ranking scores.
+
+Ordino betterScores in ordine crescente
+Se a - b restituisce un numero negativo, a viene ordinato prima di b.
+Se a - b restituisce zero, l'ordine di a e b rimane invariato.
+Se a - b restituisce un numero positivo, a viene ordinato dopo b.
+ */
 betterScores.sort((a,b) => a-b);
+
+// elimina i primi due elementi dell'array, ossia quelli più piccoli
 betterScores.shift();
 betterScores.shift();
 
+//calcolo la media su better score
 let avg = 0;
 for(const s of betterScores) {
   avg += s;
 }
 avg /= betterScores.length;
+
+//approssima all'intero più vicino
 avg = Math.round(avg);
 
+/* Add `NN+2` new scores, at the end of the array, with a 
+value equal to the (rounded) average of the existing scores.*/
 for(let i=0; i<NN+2; i++) {
   betterScores.push(avg);
 }
 
+/*
+Print both arrays, comparing the scores before 
+and after the "improvement," and showing the 
+averages in both cases.
+ */
 console.log(scores);
 console.log(betterScores);
 
--- week01/exercises/users-list.js
+++ week01/exercises/users-list.js
@@ -1,18 +1,33 @@
-'use strict' ;
+'use strict';
 
 const names = `Luigi De Russis, Luca Mannella, 
 Fulvio Corno, Juan Pablo Saenz Moreno, 
 Luca Pezzolla`;
-// esempio con ``: se le usate invece di '', potete avere strighe multi-linea: in quel caso, dovete gestire anche gli a capo, -- vedere sotto
+// esempio con ``: se le usate invece di '', potete avere strighe multi-linea: in quel caso, dovete gestire 
+// anche gli a capo, -- vedere sotto
 
 // togliamo gli a capo
+/*
+names.replace(/\n/g, ' ') utilizza il metodo replace di JavaScript per sostituire tutte le occorrenze 
+di un pattern specifico in una stringa.
+/\n/g è una regex dove:
+- \n: Rappresenta un carattere di nuova linea.
+- /.../g: il flag g (globale) indica che la sostituzione deve avvenire per tutte
+ le occorrenze del pattern nella stringa, non solo la prima.
+' ': Questo è il valore con cui ogni occorrenza del pattern viene sostituita, in questo caso uno spazio.
+
+const names = "Alice\nBob\nCharlie\nDiana"; ---> const inlineNames = "Alice Bob Charlie Diana"; 
+
+ */
 const inlineNames = names.replace(/\n/g, ' ');
 
 // creazione dell'array dei nomi
+// il metodo split in questo caso identifica ogni elemento dell'array ogni qualvolta trovi una virgola
 const nameArray = inlineNames.split(',');
 
 // rimozione degli spazi intorno alla virgola
-for(let i =0; i < nameArray.length; i++)
+for(let i = 0; i < nameArray.length; i++)
+  //trim elimina gli spazi iniziali e finali
   nameArray[i] = nameArray[i].trim();
 
 // ALTERNATIVA
@@ -25,6 +40,7 @@ for(let [i, n]  of nameArray.entries())
 const acronyms = [];
 
 for(const name of nameArray) {
+
   // ogni parola in una posizione dell'array
   const words = name.split(' ');
   let initials = '';
@@ -38,6 +54,8 @@ for(const name of nameArray) {
 }
 
 // stampo il risultato
+acronyms.sort();
+nameArray.sort ();
 for(let i=0; i < nameArray.length; i++) {
     console.log(`${acronyms[i]} - ${nameArray[i]}`);
 }
